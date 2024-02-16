485c18dc31354f6ae0de3b93e1a39cd402046e1b
Tabarin01 <119614157+Tabarin01@users.noreply.github.com>
2024-02-16 12:30:23 +0100

    Creazione LoginService

--- src/main/java/com/Scai/Project/controllers/LoginController.java
+++ src/main/java/com/Scai/Project/controllers/LoginController.java
@@ -1,10 +1,15 @@
 package com.Scai.Project.controllers;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.Scai.Project.services.LoginService;
+
 @Controller
-public class LoginController
-{
+public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
     
+
 }
