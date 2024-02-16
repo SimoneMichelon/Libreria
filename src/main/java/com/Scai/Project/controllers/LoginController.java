4f5aa9d104c188e2f906b90929631d1a85e88cf2
Tabarin01 <119614157+Tabarin01@users.noreply.github.com>
2024-02-16 12:39:45 +0100

    Mapping LP

--- src/main/java/com/Scai/Project/controllers/LoginController.java
+++ src/main/java/com/Scai/Project/controllers/LoginController.java
@@ -2,14 +2,22 @@ package com.Scai.Project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.Scai.Project.services.LoginService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
 
     @Autowired
     private LoginService loginService;
-    
+
+    @GetMapping("/loginPage")
+    public String loginPage(HttpSession session) {
+        return "loginPage.html";
+
+    }
 
 }
